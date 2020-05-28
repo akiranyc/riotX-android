5e77c2149b0bcb0016fd342a9fcc84bee091a54d
Benoit Marty <benoitm@matrix.org>
2020-05-28 14:15:35 +0200

    Create RoomCallService

--- vector/src/main/java/im/vector/riotx/features/home/room/detail/RoomDetailViewModel.kt
+++ vector/src/main/java/im/vector/riotx/features/home/room/detail/RoomDetailViewModel.kt
@@ -360,8 +360,8 @@ class RoomDetailViewModel @AssistedInject constructor(
         R.id.resend_all          -> timeline.failedToDeliverEventCount() > 0
         R.id.clear_all           -> timeline.failedToDeliverEventCount() > 0
         R.id.open_matrix_apps    -> session.integrationManagerService().isIntegrationEnabled()
-        R.id.voip_call           -> room.roomSummary()?.isDirect == true && room.roomSummary()?.joinedMembersCount == 2
-        else                     -> false
+        R.id.voice_call, R.id.video_call -> room.canStartCall()
+        else                             -> false
     }
 
 // PRIVATE METHODS *****************************************************************************
